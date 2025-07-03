6357db15e0113debf6f4a7f21cc40a2c0047dabd
Enes Malik Arı <enesmalik1245@gmail.com>
2025-07-03 16:18:31 +0300

    Fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
